6b12ba521a8f672f75b3224d5e5fc2f062562436
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-10 20:57:03 +0500

    dashboard are set

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+@section('title', 'Dashboard')
+@section('content')
+<div class="container">
+    <div class="card border-0 shadow my-5">
+         <div class="card-header bg-light">
+             <h3 class="h5 pt-2">Dashboard</h3>
+         </div>
+         <div class="card-body">
+             You are logged in !!
+         </div>
+    </div>
+ </div>
+@endsection
