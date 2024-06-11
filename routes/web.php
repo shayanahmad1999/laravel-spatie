80231e9f24c83f08acafd4a8053ee266f261f7f4
Shayan Ahmad <shayanahmad235@gmail.com>
2024-06-11 14:46:31 +0500

    restrict web route

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,17 @@ Route::group(['middleware' => 'guest'], function(){
 
     //we define role here as well and in controller as well and in blade as well define it where you want
 
-// Route::group(['middleware' => ['role:Super Admin']], function(){
+// Route::group(['middleware' => ['auth','role:Super Admin|admin']], function(){
 Route::group(['middleware' => ['auth']], function(){
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
 
-    Route::get('permission/{permission?}', [PermissionController::class, 'index'])->name('permission.index');
+    Route::get('permission/{permission?}', [PermissionController::class, 'index'])->name('permission.index')->middleware('role:Super Admin');
     Route::post('permission/store', [PermissionController::class, 'store'])->name('permission.store');
     Route::post('permission/update/{permission}', [PermissionController::class, 'update'])->name('permission.update');
     Route::delete('permission/delete/{permission}', [PermissionController::class, 'destroy'])->name('permission.destroy');
 
-    Route::get('role/{role?}', [RoleController::class, 'index'])->name('role.index');
+    Route::get('role/{role?}', [RoleController::class, 'index'])->name('role.index')->middleware('role:Super Admin');
     Route::post('role/store', [RoleController::class, 'store'])->name('role.store');
     Route::post('role/update/{role}', [RoleController::class, 'update'])->name('role.update');
     Route::delete('role/delete/{role}', [RoleController::class, 'destroy'])
